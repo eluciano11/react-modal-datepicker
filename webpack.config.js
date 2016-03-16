7038964f2c9fdfcaac7fa67f0e4d8c968a459dcb
Emmanuel Luciano <emma.luciano11@gmail.com>
2016-03-15 23:55:22 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './lib/index.js',
+
+  output: {
+    library: 'ModalDatePicker',
+    libraryTarget: 'umd',
+    path: 'dist',
+    filename: 'react-modal-datapicker.js'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: [
+          path.resolve(__dirname, 'lib', 'styles')
+        ]
+      },
+      {
+        test: /\.js/,
+        loaders: ['babel'],
+        include: [path.resolve(__dirname, 'lib')]
+      }
+    ]
+  }
+};
